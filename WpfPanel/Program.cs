af53a634a22ffdadbf86396e35caa8391c87d98b
Dzmitry <dmitrymaslakov@yandex.ru>
2023-06-16 15:46:34 +0300

    Create a project for learning

---
+++ WpfPanel/Program.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WpfPanel.ViewModel;
+
+namespace WpfPanel
+{
+    /*public class WpfController : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // nothing to clean up in this simple case
+            return Result.Succeeded;
+        }
+
+    }*/
+
+
+    [Transaction(TransactionMode.Manual)]
+    public class Program : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var viewModel = new UIViewModel();
+            var ui = new View.UI(viewModel);
+            ui.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ WpfPanel/ViewModel/UIViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfPanel.Domain;
+
+namespace WpfPanel.ViewModel
+{
+    public class UIViewModel : ViewModelBase
+    {
+        private string _message;
+
+        public UIViewModel()
+        {
+            Greeting = new GreetingCommand(this);
+        }
+
+        public string Message
+        {
+            get => _message;
+            set => Set(ref _message, value);
+        }
+        public ICommand Greeting 
+        { 
+            get; 
+            set; 
+        }
+    }
+}
